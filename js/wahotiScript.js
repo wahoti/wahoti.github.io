4339ca6c4ae6a65e5787eaddef65261ad772ae94
abdulwahed <wahedi.abdulwahed@gmail.com>
2017-09-20 10:31:08 -0700

    ?

--- js/wahotiScript.js
+++ js/wahotiScript.js
@@ -18,3 +18,18 @@ function makeABox() {
 	var boxer = document.getElementById("boxer");
 	insertAfter(boxer, newBox);
 }
+
+function startDarkSquares(){
+	var update_interval = setInterval(
+		function()
+		{
+			for(var f in acts){ f(); }
+			for(var x in objects){ objects[x].step(); }
+			
+		}
+	, 16);
+
+	var draw_interval = setInterval( function() {	
+		console.log('draw');
+	} , 16);
+}
