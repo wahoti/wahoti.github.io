fd5ce702b653a5870734ce1872fd49ff82b4101d
abdulwahed <wahedi.abdulwahed@gmail.com>
2017-09-20 01:01:06 -0700

    tabs + pictures

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ app.get('/js/three.min.js', function(req,res) {
 	res.sendFile(__dirname + '/js/three.min.js');
 });
 
+app.get('/res/linkedinpic.jpg', function(req,res) {
+	res.sendFile(__dirname + '/res/linkedinpic.jpg');
+});
+
+app.get('/res/symphony.png', function(req,res) {
+	res.sendFile(__dirname + '/res/symphony.png');
+});
+
 http.listen(app.get('port'), function() {
 	console.log('listening on ' + app.get('port') + '...');
 });
