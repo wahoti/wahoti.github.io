0742979681bc4cd7d45854d69a0114bd75148214
abdulwahed <wahedi.abdulwahed@gmail.com>
2017-11-15 15:00:35 -0800

    fix wall

--- main.js
+++ main.js
@@ -1596,7 +1596,7 @@ var dude_list = {
 	},
 	wall: object = {
 		description: "wall:<br/> can't move can be captured.",
-		tag: '^^^',
+		tag: '---',
 		mobility: false,
 		is_piece: false,
 		movement_patterns: [],
